dfdab03883aedf1d2992c597fb62e326b7d1ddfa
PallaviPatel <153287259+pallavi-patel-developer@users.noreply.github.com>
2026-03-07 01:00:30 +0530

    Update BACKEND_URL for production environment

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ if (typeof window !== 'undefined') {
   // Set BACKEND_URL based on hostname (Local vs Production)
   window.process.env.BACKEND_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000'
-    : 'https://portpholiohub.onrender.com';
+    : 'https://portfolio-back-u5qn.onrender.com';
 }
